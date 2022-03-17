484efe1226ea1ca866b5abc98d3c797adfd8afaa
Sourajit Banerjee <sourajitbanerjee2015@gmail.com>
2022-03-17 23:26:35 +0530

    mailing service transporter fixed

--- nodemailer/comments_mailer.js
+++ nodemailer/comments_mailer.js
@@ -1,22 +1,22 @@
 //const nodeMailer = require('../config/nodemailer');
-// const nodemailer = require('../config/nodemailer');
+const nodemailer = require('../config/nodemailer');
 
-// exports.newComment = (comment) => {
-//     console.log('Inside newComment mailer',comment);
+exports.newComment = (comment) => {
+    console.log('Inside newComment mailer',comment);
 
-//     let htmlString = nodemailer.renderTemplate({comment:comment},'/comments/new_comment.ejs');
+    let htmlString = nodemailer.renderTemplate({comment:comment},'/comments/new_comment.ejs');
 
-//     nodemailer.transporter.sendMail({
-//         from : 'Codehub@gmail.com',
-//         to : comment.user.email,
-//         subject : 'New Comment Published',
-//         html : htmlString
-//     }, (err,info) => {
-//         if(err){
-//             console.log('error in sending mail',err);
-//             return;
-//         }
-//         console.log('Message Sent',info);
-//         return;
-//     });
-// }
+    nodemailer.transporter.sendMail({
+        from : 'Codehub@gmail.com',
+        to : comment.user.email,
+        subject : 'New Comment Published',
+        html : htmlString
+    }, (err,info) => {
+        if(err){
+            console.log('error in sending mail',err);
+            return;
+        }
+        console.log('Message Sent',info);
+        return;
+    });
+}
